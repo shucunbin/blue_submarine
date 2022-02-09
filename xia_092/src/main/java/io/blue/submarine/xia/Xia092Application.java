4aba13ac7d6723c40f8110aa034f61197833730d
cunbin.shu <cunbin.shu@mail.dealmoon.com>
2022-02-09 14:22:27 +0800

    实验性工程

---
+++ xia_092/src/main/java/io/blue/submarine/xia/Xia092Application.java
@@ -0,0 +1,18 @@
+package io.blue.submarine.xia;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xia092Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Xia092Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ xia_092/src/test/java/io/blue/submarine/xia/Xia092ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.blue.submarine.xia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Xia092ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
